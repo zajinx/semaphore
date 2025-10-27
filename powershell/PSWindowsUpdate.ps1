e27a38b110313e3f5f05cafa54b33545ac65af8b
zajinx <matei.georgian04@gmail.com>
2025-10-27 15:31:20 +0200

    Create PSWindowsUpdate.ps1

---
+++ powershell/PSWindowsUpdate.ps1
@@ -0,0 +1,5 @@
+# Install the module from the PowerShell Gallery
+Install-Module -Name PSWindowsUpdate -Force -AcceptLicense
+
+# Import the module into your current session to start using it
+Import-Module PSWindowsUpdate
